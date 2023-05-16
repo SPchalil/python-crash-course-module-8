eb29542a182ebfedcb2ba3a3255d0a7735f7393e
SPchalil <74065235+SPchalil@users.noreply.github.com>
2023-05-16 22:23:03 +0000

    Added a text file
    Added a text file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+text_inputted = str(input("Start typing: "))
+print("\n")
+file_name = str(input("Enter file name and press ENTER: ")) + '.txt'
+text_file = open(file_name, 'x')  # Will create a new file with the name
+        
+text_file = open(file_name, 'a')
+text_file = text_file.write(
+text_inputted)  #Write() command is used to write to file
+  
+print("\n")
+print("File created.")
